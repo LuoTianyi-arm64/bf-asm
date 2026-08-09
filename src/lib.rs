c26067a50274ee59cca76239734e795d3fde4a85
LuoTianyi-arm64 <2153585992@qq.com>
2026-08-09 20:45:12 +0800

    Developing

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,18 @@
 
 pub mod asm;
 pub use asm::*;
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+    #[test]
+    fn test0() {
+        let mut bf_code = String::new();
+        bf_asm!(mov ram 1, input, target bf_code);
+        bf_asm!(mov ram 2, ram 3, from_ram 1, target bf_code);
+        bf_asm!(mov output, ram 2, ram 3, target bf_code);
+        bf_asm!(mov ram 2, ram 3, number 2, target bf_code);
+        simplify_bf!(code bf_code, target bf_code);
+        assert_eq!(bf_code, ">,>[-]>[-]<<[->+>+<<]>.>.")
+    }
+}
