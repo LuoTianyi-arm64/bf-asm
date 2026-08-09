d2e1ddfd92bde3a90eac4fc164ac155be2e883e9
LuoTianyi-arm64 <2153585992@qq.com>
2026-08-09 16:48:56 +0800

    Developing

--- src/asm.rs
+++ src/asm.rs
@@ -4,7 +4,7 @@
 pub static mut POINT: usize = 0;
 
 #[macro_export] macro_rules! bf_asm {
-    (mov r $addr: expr, in, tar $target: ident) => {
+    (mov ram $addr: expr, input, target $target: ident) => {
         unsafe {
             let mut output = String::new();
             if POINT > $addr {
@@ -21,7 +21,7 @@ pub static mut POINT: usize = 0;
             $target.push_str(&output);
         }
     };
-    (mov out $(, r $addr: expr)+ , tar $target: ident) => {
+    (mov output $(, ram $addr: expr)+ , target $target: ident) => {
         $(unsafe {
             let mut output = String::new();
             if POINT > $addr {
@@ -39,7 +39,7 @@ pub static mut POINT: usize = 0;
         })+
 
     };
-    (mov $(r $addr0:expr ,)+ fr $addr1: expr , tar $target: ident) => {
+    (mov $(ram $addr0:expr ,)+ from_ram $addr1: expr , target $target: ident) => {
         $(assert!($addr0 != $addr1, "错误,源和目标不能相同");)+
         let mut output = String::new();
         $(unsafe {
@@ -74,7 +74,7 @@ pub static mut POINT: usize = 0;
         }
         $target.push_str(&output);
     };
-    (mov $(r $addr: expr ,)+ n $num: expr $(, tmp $tmp:expr)? , tar $target: ident) => {
+    (mov $(ram $addr: expr ,)+ number $num: expr $(, tmp $tmp:expr)? , target $target: ident) => {
         let tmp = {
             let tmp:Option<usize> = None;
             $(
@@ -105,7 +105,7 @@ pub static mut POINT: usize = 0;
             }
             Some(t) => {
                 // 仅用于缩短bf代码长度, 并不会提高运行速度
-                let pre_com = [[3, 5, 0], [4, 4, 0], [4, 4, 1], [3, 6, 0], [3, 6, 1], [4, 5, 0], [3, 7, 0], [3, 7, 1], [4, 6, -1], [4, 6, 0], [5, 5, 0], [5, 5, 1], [5, 5, 2], [4, 7, 0], [4, 7, 1], [5, 6, 0], [5, 6, 1], [4, 8, 0], [4, 8, 1], [5, 7, -1], [5, 7, 0], [6, 6, 0], [6, 6, 1], [6, 6, 2], [5, 8, -1], [5, 8, 0], [5, 8, 1], [6, 7, 0], [6, 7, 2], [5, 9, -1], [5, 9, 0], [5, 9, 1], [6, 8, -1], [6, 8, 0], [6, 8, 1], [5, 10, 0], [5, 10, 1], [5, 10, 2]];
+                let pre_com = [[3, 5, 0], [4, 4, 0], [4, 4, 1], [3, 6, 0], [3, 6, 1], [4, 5, 0], [3, 7, 0], [3, 7, 1], [4, 6, -1], [4, 6, 0], [5, 5, 0], [5, 5, 1], [5, 5, 2], [4, 7, 0], [4, 7, 1], [5, 6, 0], [5, 6, 1], [4, 8, 0], [4, 8, 1], [5, 7, -1], [5, 7, 0], [6, 6, 0], [6, 6, 1], [6, 6, 2], [5, 8, -1], [5, 8, 0], [5, 8, 1], [6, 7, 0], [6, 7, 2], [5, 9, -1], [5, 9, 0], [5, 9, 1], [6, 8, -1], [6, 8, 0], [6, 8, 1], [5, 10, 0], [5, 10, 1], [5, 10, 2], [6, 9, -1], [6, 9, 0], [6, 9, 1], [7, 8, 0], [7, 8, -1]];
                 todo!()
             }
         }
--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,10 @@ use bf_asm::*;
 
 fn main() {
     let mut bf_code = String::new();
-    bf_asm!(mov r 1, in, tar bf_code);
-    bf_asm!(mov r 2, r 3, fr 1, tar bf_code);
-    bf_asm!(mov out, r 2, r 3, tar bf_code);
-    bf_asm!(mov r 2, r 3, n 2, tar bf_code);
+    bf_asm!(mov ram 1, input, target bf_code);
+    bf_asm!(mov ram 2, ram 3, from_ram 1, target bf_code);
+    bf_asm!(mov output, ram 2, ram 3, target bf_code);
+    bf_asm!(mov ram 2, ram 3, number 2, target bf_code);
     simplify_bf!(code bf_code, target bf_code);
     println!("{bf_code}");
 }
